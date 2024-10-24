f93da457920680fbd3127815813bac52edea26a7
Ahmed Mohmmedd <ahmedmkersha22@gmail.com>
2024-10-24 10:26:21 +0300

    merge calls brance

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         textTheme: GoogleFonts.interTextTheme(),
       ),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
